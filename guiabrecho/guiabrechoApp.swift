7d7251a0ae2475b8e5b764f9dcf751ed3d8ad82d
Thalyta Souza <mayerflot@gmail.com>
2023-12-12 11:39:04 -0300

    Initial Commit

---
+++ guiabrecho/guiabrechoApp.swift
@@ -0,0 +1,17 @@
+//
+//  guiabrechoApp.swift
+//  guiabrecho
+//
+//  Created by Thalytinha on 12/12/23.
+//
+
+import SwiftUI
+
+@main
+struct guiabrechoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
